b31652f910db6522030d1813fce812292b8a22c9
James Jelenko <jamesjelenko@gmail.com>
2024-05-25 22:01:51 -0700

    add initial server impl

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+let users = {};
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('login', (userId) => {
+    users[userId] = socket.id;
+  });
+
+  socket.on('call', (data) => {
+    io.to(users[data.to]).emit('call', {
+      from: data.from,
+      signal: data.signal,
+    });
+  });
+
+  socket.on('answer', (data) => {
+    io.to(users[data.to]).emit('answer', {
+      signal: data.signal,
+      from: data.from,
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('message', (data) => {
+    io.emit('message', data);
+  });
+});
+
+http.listen(3001, () => {
+  console.log('listening on *:3001');
+});
