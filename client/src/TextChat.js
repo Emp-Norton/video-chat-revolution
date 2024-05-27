7ae555c7eafd81346ae47b9fadeca928344894d6
James Jelenko <jamesjelenko@gmail.com>
2024-05-26 23:38:26 -0700

    add check for socket existing, move magic string into a constants file

--- client/src/TextChat.js
+++ client/src/TextChat.js
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
 import { useSocket } from './SocketContext';
+import { SOCKET_URL } from "./constants";
 
 const TextChat = ({ room }) => {
-    const socket = useSocket();
+    const socket = useSocket(SOCKET_URL);
+    console.log(`Socket: ${socket}\nURL: ${SOCKET_URL}`)
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
 
     const sendMessage = () => {
+        if (!socket.socket) return
         socket.emit('message', { room, message });
         setMessages([...messages, message]);
         setMessage('');
     };
-    if (socket) {
+    if (!socket.socket) return
+        debugger;
         socket.on('message', (message) => {
             setMessages((msgs) => [...msgs, message]);
         });
-    }
+
     return (
         <div>
             <h2>Text Chat</h2>
