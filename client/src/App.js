05fe5d86111fd66f405061d1b7876a6701d7e5c3
James Jelenko <jamesjelenko@gmail.com>
2024-05-26 23:40:06 -0700

    pull VideoChat component into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import SocketProvider from './SocketContext';
 import TextChat from './TextChat';
 import GlobalChat from './GlobalChat';
+import VideoChat from './VideoChat';
 
 const App = () => {
-  return (
-      <SocketProvider>
-        <div>
-          <h1>Video Chat App</h1>
-          <TextChat room="room1" />
-          <GlobalChat />
-        </div>
-      </SocketProvider>
-  );
+    return (
+        <SocketProvider>
+            <div>
+                <h1>Video Chat App</h1>
+                <TextChat room="room1" />
+                <GlobalChat />
+                <VideoChat room="room1" />
+            </div>
+        </SocketProvider>
+    );
 };
 
 export default App;
